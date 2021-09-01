ed86654a1c4ecce25817d5f487a5da00a8ccae7d
Michal Rolirad <romi9987@gmail.com>
2021-09-01 17:24:45 +0200

    j'ai ajouté thymeleaf et th annotations @ et $ dans articles.html

--- SpringStockMvcSec2/src/main/java/fr/ldnr/web/ArticleController.java
+++ SpringStockMvcSec2/src/main/java/fr/ldnr/web/ArticleController.java
@@ -1,18 +1,31 @@
 package fr.ldnr.web;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import fr.ldnr.dao.ArticleRepository;
+import fr.ldnr.entities.Article;
 
 @Controller
 public class ArticleController {
 	@Autowired
 	ArticleRepository articleRepository;
 
+//	@GetMapping("/index")
+//	public String index() {
+//		return ("articles");
+//	}
+	
 	@GetMapping("/index")
-	public String index() {
-		return ("articles");
-	}
+		public String index(Model model) {
+			List<Article> articles = articleRepository.findAll();
+			
+			model.addAttribute("listArticle", articles); 
+														
+			return "articles";
+		}
 }
