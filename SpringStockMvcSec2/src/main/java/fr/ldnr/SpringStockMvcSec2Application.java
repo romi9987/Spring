d96d6aab02998d41a1962180945927aa4463c5e2
Michal Rolirad <romi9987@gmail.com>
2021-09-01 14:43:15 +0200

    Initial commit

---
+++ SpringStockMvcSec2/src/main/java/fr/ldnr/SpringStockMvcSec2Application.java
@@ -0,0 +1,13 @@
+package fr.ldnr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStockMvcSec2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringStockMvcSec2Application.class, args);
+	}
+
+}
--- com.vogella.git.first/src/com/vogella/git/first/GitTest.java
+++
@@ -1,8 +0,0 @@
-package com.vogella.git.first;
-
-public class GitTest {
-	public static void main(String[] args) {
-		System.out.println("Git is cool");
-	}
-
-}
