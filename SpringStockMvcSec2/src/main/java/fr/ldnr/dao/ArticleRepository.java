7a5bf4655e3e1565d448f1bdcc1c2eb587871673
Michal Rolirad <romi9987@gmail.com>
2021-09-01 21:00:48 +0200

    Etape 9 - Utilisation de Spring Data

--- SpringStockMvcSec2/src/main/java/fr/ldnr/dao/ArticleRepository.java
+++ SpringStockMvcSec2/src/main/java/fr/ldnr/dao/ArticleRepository.java
@@ -1,10 +1,12 @@
 package fr.ldnr.dao;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import fr.ldnr.entities.Article;
 
 public interface ArticleRepository extends JpaRepository<Article, Long>{
-	
+	Page<Article> findByDescriptionContains(String description, Pageable pageable);
 
 }
